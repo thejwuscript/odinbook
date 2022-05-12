18fef7bd0242730bf96dd581473f0dee278e69f4
James Wu <ichangjwu@outlook.com>
2022-05-13 03:03:30 +0900

    Improve UI for friend request

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,5 +2,6 @@ class FriendsController < ApplicationController
   def index
     @friends_list = current_user.friends
     @not_friends_list = current_user.not_friends
+    @potential_friends = FriendRequest.where(requester_id: current_user.id).map { |request| request.requestee }
   end
 end
