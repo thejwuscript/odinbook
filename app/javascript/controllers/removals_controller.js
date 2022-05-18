0b54c99782016e2698e04350cdccb713dc55a552
James Wu <ichangjwu@outlook.com>
2022-05-19 00:35:57 +0900

    Add stimuluscontroller to flash

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -1,10 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static target = [ "flash" ]
 
   remove() {
-    const element = this.flashTarget
-    element.remove()
+    this.element.remove()
   }
 }
