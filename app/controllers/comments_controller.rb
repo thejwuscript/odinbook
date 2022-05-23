c5a0ac61e7c0649b6aec7586fb31e512e6212376
James Wu <ichangjwu@outlook.com>
2022-05-24 04:14:17 +0900

    Create new comment text field

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
+  skip_before_action :count_friend_requests
+
   def index
   end
   
   def new
     @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(author: current_user)
     render 'posts/comments/new'
   end
 
