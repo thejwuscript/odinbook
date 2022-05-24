e16ed2fe9b6f5a65cad2e5bc5055951d50c4c92c
James Wu <ichangjwu@outlook.com>
2022-05-24 22:34:03 +0900

    Add callback on Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,15 @@
 class Friendship < ApplicationRecord
+  after_destroy do |friendship|
+    Friendship.where(friend_id: friendship.user.id).delete_all
+  end
+
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
   validates :friend, uniqueness: { scope: :user }
+
+  #def destroy
+  #  list = Friendship.where(user_id: user.id).or(Friendship.where(friend_id: user.id))
+  #  list.delete_all
+  #end
 end
