bd14864a0dd1164a2a8ccedeb365932ec7b8ea7c
James Wu <ichangjwu@outlook.com>
2022-05-25 17:58:18 +0900

    Add validation to avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,19 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
+  validate :avatar_format
 
   def avatar_as_thumb
     avatar.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  private
+
+  def avatar_format
+    return unless avatar.attached?
+    return if avatar.content_type.in?(['image/jpeg', 'image/png'])
+    
+    errors.add(:avatar, 'Needs to be an image in .jpeg or .png format')
   end
 
 end
