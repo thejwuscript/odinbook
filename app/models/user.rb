5d4b787da93e39c084b6ca4715340dff6de2bfa7
James Wu <ichangjwu@outlook.com>
2022-05-29 17:09:08 +0900

    Revert "Edit callback function to attach fb avatar"
    This reverts commit 520722d19f60a602cbb3d65628ee95888bca1adb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,9 +38,12 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.username = auth.info.name
+      user.username = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
     end
-
   end
 
   def self.new_with_session(params, session)
