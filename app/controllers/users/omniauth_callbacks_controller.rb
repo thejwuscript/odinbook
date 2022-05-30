1cfe8104637cb468bb7a0361251a9560584a1758
James Wu <ichangjwu@outlook.com>
2022-05-30 17:44:27 +0900

    Create new profile for new FB users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.create_profile(request.env["omniauth.auth"]) if Profile.find_by(user_id: @user).nil?
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
