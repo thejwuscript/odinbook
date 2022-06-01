01c7c1829311b51542b59a8bb4ee48c6f2c7f61a
James Wu <ichangjwu@outlook.com>
2022-06-01 19:43:40 +0900

    Add image_url column to Posts

---
+++ db/migrate/20220601100729_add_image_url_to_post.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToPost < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_155110) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_100729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_155110) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
