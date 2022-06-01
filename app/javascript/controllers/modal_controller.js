7a101e21fd125fb5461c143af419c0752412da68
James Wu <ichangjwu@outlook.com>
2022-06-02 02:55:12 +0900

    Fix closing modal behaviour with pointer events

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = [ "modal" ]
+  static targets = [ "modal", "input", "output", "modaldialog" ]
 
   show(event) {
     let element = this.modalTarget;
@@ -15,6 +15,27 @@ export default class extends Controller {
       event.preventDefault();
       let element = this.modalTarget;
       element.style.display = "none";
+      this.modalTarget.setAttribute('data-action', 'mousedown->modal#addClickAction');
     };
+  }
+
+  copy(event) {
+    event.preventDefault();
+    let url = this.inputTarget.value;
+    this.modalTarget.style.display = "none";
+    this.outputTarget.innerHTML = `<img src=${url} class="preview-avatar">`;
+  };
+
+  addClickAction() {
+    const element = this.modalTarget
+    element.setAttribute('data-action', 'click->modal#close');
+  }
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  removeClickAction() {
+    this.modalTarget.setAttribute('data-action', 'mousedown->modal#addClickAction');
   }
 }
