47282ebbdde07e58ae3c312d7a4c5f622afe5ba7
James Wu <ichangjwu@outlook.com>
2022-06-02 19:52:55 +0900

    Optimize friendship queries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all_except(current_user)
+    @users = User.all_except(current_user).order(:username)
     @friends_list = current_user.friends
     @not_friends_list = current_user.not_friends
-    @users_sent_requests_to = FriendRequest.where(requester_id: current_user.id).map { |request| request.requestee }
-    @users_received_requests_from = FriendRequest.where(requestee_id: current_user.id).map { |request| request.requester }
+    @users_sent_requests_to = FriendRequest.where(requester_id: current_user.id).includes(:requestee).map { |request| request.requestee }
+    @users_received_requests_from = FriendRequest.where(requestee_id: current_user.id).includes(:requester).map { |request| request.requester }
     @potential_friends = @users_sent_requests_to + @users_received_requests_from
   end
 
