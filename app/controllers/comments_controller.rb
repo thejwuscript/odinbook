29988d9c488f7feea67c40825b5bcaaa09e19cf6
James Wu <ichangjwu@outlook.com>
2022-09-15 14:52:42 +0900

    Add javascript to build comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,18 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(author: current_user)
     @post_comments = @post.comments.all.order(created_at: :desc)
+    @post_comments_with_name = @post_comments.to_a.map do |comment|
+      hash = {}
+      hash[:author] = comment.author.name
+      hash[:body] = comment.body
+      hash
+    end
     @avatar = current_user.avatar
-    render "posts/comments/new"
+    # render "posts/comments/new"
+    respond_to do |format|
+      format.json { render json: {name: current_user.name, post: @post, imageUrl: url_for(@avatar), newComment: @comment, postComments: @post_comments_with_name}}
+      format.html { render "posts/comments/new" }
+    end
   end
 
   def create
