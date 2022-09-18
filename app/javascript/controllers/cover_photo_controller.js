ac0d970baed37e8287131ce43f2925ff66199f4b
James Wu <ichangjwu@outlook.com>
2022-09-18 21:18:09 +0900

    Fix edit button

--- app/javascript/controllers/cover_photo_controller.js
+++ app/javascript/controllers/cover_photo_controller.js
@@ -5,16 +5,15 @@ export default class extends Controller {
   static targets = [ "menu", "editButton" ];
 
   connect() {
-    const menu = this.menuTarget;
-    menu.style.display = "none";
   }
 
-  toggle() {
-    const menu = this.menuTarget;
-    if (menu.style.display === "none") {
-      menu.style.display = "flex";
-    } else {
-      menu.style.display = "none";
+  toggle(e) {
+    e.stopPropagation();
+    this.menuTarget.classList.toggle("visible");
+    if (this.menuTarget.classList.contains("visible")) {
+      document.body.addEventListener('click', () => {
+        this.menuTarget.classList.remove('visible');
+      })
     }
   }
 
