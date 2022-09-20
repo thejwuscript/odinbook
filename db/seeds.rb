0946ad9ad45ba4488b719792093bf2e4db112264
James Wu <ichangjwu@outlook.com>
2022-09-20 15:16:15 +0900

    Add link to users and more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 USER_PASSWORD = 'password'.freeze
 
-2.times do
+6.times do
   user = User.create!(Faker::Internet.unique.user.merge({ password: USER_PASSWORD,
                                                           password_confirmation: USER_PASSWORD }))
   image = Down.download(Faker::Avatar.unique.image(size: '300x300'))
   user.profile.avatar.attach(io: image, filename: "avatar#{user.profile.id}#{Time.current.hash}")
   user.profile.update!(display_name: Faker::Name.unique.name)
+  3.times do
+    user.posts.create(body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4))
+  end
 end
 
 admin = User.create!(username: 'admin', email: 'admin@example.com', password: USER_PASSWORD,
