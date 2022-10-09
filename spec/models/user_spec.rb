379b463f9a53da7bd61c6a1bf463ead503f06857
James Wu <ichangjwu@outlook.com>
2022-10-09 18:04:32 +0900

    Add testing associations to user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,17 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_length_of(:username).is_at_most(50) }
     it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(128) }
   end
+
+  context 'testing associations' do
+    it { is_expected.to have_one(:profile).dependent(:destroy) }
+    it { is_expected.to have_many(:sent_requests).class_name(:FriendRequest).dependent(:destroy) }
+    it { is_expected.to have_many(:received_requests).class_name(:FriendRequest).dependent(:destroy) }
+    it { is_expected.to have_many(:friendships) }
+    it { is_expected.to have_many(:friends).through(:friendships) }
+    it { is_expected.to have_many(:posts).dependent(:destroy) }
+    it { is_expected.to have_many(:likes).dependent(:destroy) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many(:sent_notifications).class_name(:Notification).dependent(:destroy) }
+    it { is_expected.to have_many(:received_notifications).class_name(:Notification).dependent(:destroy) }
+  end
 end
