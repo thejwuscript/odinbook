719e2f288e51b9e08e6f46b7450396b9fd674d1c
James Wu <ichangjwu@outlook.com>
2022-10-10 02:54:38 +0900

    Use faker gem with factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { 'test_email@example.com' }
-    username { 'test_username@example.com' }
+    email { Faker::Internet.safe_email }
+    username { Faker::Internet.username(specifier: 6..12) }
     password { '123456' }
     password_confirmation { '123456' }
   end
