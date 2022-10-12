dabda4d7f57794a487aee82488d6504369ff7ad8
James Wu <ichangjwu@outlook.com>
2022-10-13 03:03:05 +0900

    Fix quotes

--- spec/system/log_in_spec.rb
+++ spec/system/log_in_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'Log in', type: :system do
 
   context 'when the user fills in the correct credentials' do
     it 'logs in user and redirects to homepage', :aggregate_failures do
-      VCR.use_cassette "news headlines" do
+      VCR.use_cassette 'news headlines' do
         visit root_path
-        fill_in "user_login", with: user.login
-        fill_in "user_password", with: user.password
-        click_on "Log in"
+        fill_in 'user_login', with: user.login
+        fill_in 'user_password', with: user.password
+        click_on 'Log in'
         expect(page).to have_current_path(root_path)
         expect(page).to have_content('Signed in successfully.')
       end
@@ -19,9 +19,9 @@ RSpec.describe 'Log in', type: :system do
   context 'when the user fills in the wrong credentials' do
     it 'displays an error flash message on the same page', :aggregate_failures do
       visit root_path
-      fill_in "user_login", with: "kiekdos"
-      fill_in "user_password", with: "aaaaaaaa"
-      click_on "Log in"
+      fill_in 'user_login', with: 'kiekdos'
+      fill_in 'user_password', with: 'aaaaaaaa'
+      click_on 'Log in'
       expect(page).to have_current_path(new_user_session_path)
       expect(page).to have_content('The provided login credentials were invalid.')
     end
