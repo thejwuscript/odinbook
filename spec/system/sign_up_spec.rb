b9f917524940d8a077a0b67921cb0d414c774ebf
James Wu <ichangjwu@outlook.com>
2022-10-13 03:03:21 +0900

    Add system specs for signing up

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -1,21 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'Sign up', type: :system do
-  # create a user as subject.
-  
   context 'when the user enters and submits valid credentials' do
-    it 'signs up successfully and redirects to homepage' do
-      
+    before do
+      VCR.use_cassette 'news headlines' do
+        visit root_path
+        click_on 'Create new account'
+        fill_in 'user_username', with: 'test_user'
+        fill_in 'user_email', with: 'test_user@example.com'
+        fill_in 'user_password', with: '123456'
+        fill_in 'user_password_confirmation', with: '123456'
+        click_on 'Sign up'
+      end
+    end
+
+    it 'signs up successfully and redirects to homepage', :aggregate_failures do
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('Welcome! You have signed up successfully.')
     end
   end
 
-  context 'when the user enters and submits invalid credentials' do
-    it 'shows error messages corresponding to the invalid fields' do
-      
+  context 'when the user enter an invalid email format' do
+    before do
+      visit root_path
+      click_on 'Create new account'
+      fill_in 'user_username', with: 'test_user'
+      fill_in 'user_email', with: 's'
+      fill_in 'user_password', with: '123456'
+      fill_in 'user_password_confirmation', with: '123456'
+      click_on 'Sign up'
     end
 
-    it 'revalidates and updates error messages accordingly' do
-      
+    it 'shows a warning pop up message' do
+      message = page.find('#user_email').native.attribute('validationMessage')
+      expect(message).to include("Please include an '@' in the email address.")
+    end
+  end
+
+  context 'when the user does not fill in username and password fields' do
+    before do
+      visit root_path
+      click_on 'Create new account'
+      fill_in 'user_email', with: 'test_user@example.com'
+      click_on 'Sign up'
+    end
+
+    it 'shows error messages corresponding to the invalid fields', :aggregate_failures do
+      expect(page).to have_content("Username can't be blank")
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    it 'revalidates and updates error messages accordingly', :aggregate_failures do
+      fill_in 'user_password', with: '123'
+      click_on 'Sign up'
+      expect(page).to have_content('Password is too short')
+      expect(page).to have_content("Password confirmation doesn't match Password")
     end
   end
 end
