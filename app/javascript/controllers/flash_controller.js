fa9d288b0900279dfa5e87b400e3b953ad670ef4
James Wu <ichangjwu@outlook.com>
2022-10-16 03:21:19 +0900

    Change flash to fixed position and header to sticky

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="flash"
+export default class extends Controller {
+  connect() {
+    const header = document.querySelector('header');
+    if (header) {
+      header.style.position = 'sticky';
+      header.style.top = `${this.element.offsetHeight - 0.5}px`;
+    }  
+  }
+
+  remove() {
+    this.element.remove();
+    const header = document.querySelector('header');
+    if (header) header.style.position = 'static';
+  }
+}
