8a2c04f668377abb1eda8f37e67f5f11c978c39c
James Wu <ichangjwu@outlook.com>
2022-10-23 01:07:49 +0900

    Add attributes to Post and migrate

---
+++ db/migrate/20221022155456_add_image_url_and_image_data_url_to_posts.rb
@@ -0,0 +1,6 @@
+class AddImageUrlAndImageDataUrlToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :image_url, :string
+    add_column :posts, :image_data_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_170928) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_155456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_170928) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.string "image_data_url"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
