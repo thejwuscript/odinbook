12d1718dc6b126a53c0c0f78a44566b383a34c39
James Wu <ichangjwu@outlook.com>
2022-10-26 22:08:48 +0900

    Add new post policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,6 +59,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    authorize @post
 
     if @post.update(post_params)
       process_image_attachment
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,8 @@
 class PostPolicy < ApplicationPolicy
+  def update?
+    @record.author == current_user
+  end
+  
   def destroy?
     @record.author == @user
   end
