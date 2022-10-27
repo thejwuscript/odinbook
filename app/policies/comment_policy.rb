8e873cc7a4023fcf169d608642a9b4980f348b64
James Wu <ichangjwu@outlook.com>
2022-10-27 13:50:43 +0900

    Add comment and post policies

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,10 +1,9 @@
-class PostPolicy < ApplicationPolicy
+class CommentPolicy < ApplicationPolicy
   def update?
     @record.author == @user
   end
-  
+
   def destroy?
     @record.author == @user
   end
-  
 end
