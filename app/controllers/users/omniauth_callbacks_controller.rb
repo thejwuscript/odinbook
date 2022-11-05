40d48fd00daa4ce44b3c5b68e214ff2bedfd431a
James Wu <ichangjwu@outlook.com>
2022-11-06 01:16:19 +0900

    Reduce data added to session

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
-      session['devise.facebook_data'] = auth
+      session['devise.facebook_data'] = auth.except(:extra)
       redirect_to new_user_registration_url
     end
   end
