cee338c9cab5877556db23c23af6cf0ad82bbcb9
James Wu <ichangjwu@outlook.com>
2022-11-06 22:05:42 +0900

    Flash to not cover header and nav

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -4,15 +4,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const header = document.querySelector('header');
+    const nav = document.querySelector('.topnav');
     if (header) {
       header.style.position = 'sticky';
       header.style.top = `${this.element.offsetHeight - 0.5}px`;
-    }  
+    };
+    if (nav) {
+      nav.style.position = 'sticky';
+      nav.style.top = `${this.element.offsetHeight - 0.5}px`;
+    }
   }
 
   remove() {
     this.element.remove();
     const header = document.querySelector('header');
+    const nav = document.querySelector('.topnav');
     if (header) header.style.top = 0;
+    if (nav) nav.style.top = 0;
   }
 }
