a3042e279635f9107e5805896c57c6a663481461
James Wu <ichangjwu@outlook.com>
2023-10-04 01:33:38 +0900

    Safe email deprecated, use email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.unique.safe_email }
+    email { Faker::Internet.unique.email }
     username { Faker::Internet.unique.username(specifier: 6..12) }
     password { '123456' }
     password_confirmation { '123456' }
